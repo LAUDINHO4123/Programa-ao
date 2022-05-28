11b64cc0976aca7038d8f940861d1612e1b8e415
LAUDINHO4123 <106438292+LAUDINHO4123@users.noreply.github.com>
2022-05-28 15:13:10 +0100

    Update products.c

--- products.c
+++ products.c
@@ -8,7 +8,7 @@
 #include "products.h"
 
 
-PRODUCT get_product(products ps){
+PRODUCT get_product(products ps){ 
     return ps->product;
 }
 
@@ -24,44 +24,44 @@ void set_next(products ps,products next){
     ps->next = next;
 }
 
-products create_products(){
-    products new = malloc(sizeof(struct products));
+products create_products(){ //cria uma lista de produtos vazia
+    products new = malloc(sizeof(struct products)); //aloca o espaço
 
     return new;
 }
 
-products new_products(PRODUCT p){
+products new_products(PRODUCT p){ //cria uma lista de produtos so com um produto
     products ps = create_products();
 
-    ps->product = p;
+    ps->product = p; 
     ps->next=NULL;
 
     return ps;
 }
 
-int insertProduct (products* l, PRODUCT x,int i) {
+int insertProduct (products* l, PRODUCT x,int i) { // insere um produto ordenado,ver se e maior se n for avança e coloca la
     products list = *l;
 
-    if(i==0) {
+    if(i==0) { //insere um produto se a lista tiver vazia
         list -> product = x;
         list -> next = NULL;
 
         return 1;
     }
 
-    products new = malloc(sizeof(struct products));
+    products new = malloc(sizeof(struct products)); //aloca espaço,cria uma lista so com um produto e vai ser inserido na ordem dele
     new->product = x;
     new->next = NULL;
 
     
-    if(get_id(list->product) > get_id(x)) {
+    if(get_id(list->product) > get_id(x)) { //vai ser inserido na ordem dele,ta a procura da ordem
         *l = new;
         new->next = list;
 
         return 1;
     }
     
-    while(list->next) {
+    while(list->next) {  
         products next = list->next;
         if(get_id(next->product) > get_id(x)) {
             list->next = new;
@@ -76,10 +76,10 @@ int insertProduct (products* l, PRODUCT x,int i) {
     
 }
 
-int remove_products(int id, products* ps, int i){
+int remove_products(int id, products* ps, int i){ //remover um produto
     products p = *ps,b;
 
-    if(get_id(get_product(p))==id){
+    if(get_id(get_product(p))==id){ 
         if(i==1) p = NULL;
         if(i==2) {
             p=p->next;
@@ -106,19 +106,19 @@ int remove_products(int id, products* ps, int i){
     return 0;
 }
 
-PRODUCT search_product(int id, products ps,int i){
+PRODUCT search_product(int id, products ps,int i){ //  procura um produto
 
-    while (get_id(ps->product)<id && ps->next != NULL) ps=ps->next;
+    while (get_id(ps->product)<id && ps->next != NULL) ps=ps->next; //ta a avançar enquanto o indice for maior 
 
-    if(get_id(ps->product)==id) return ps->product;
+    if(get_id(ps->product)==id) return ps->product;  //ver se igual,se n for da return
     
 
     return NULL;
 }
 
-void build_products(char *line, products* ps,int i){
-    PRODUCT p = build_product(line);
+void build_products(char *line, products* ps,int i){ 
+    PRODUCT p = build_product(line); //  constroi um produto dado a linha
     
-    insertProduct(ps, p,i);
+    insertProduct(ps, p,i);//  insere o produto na lista
 
 }
